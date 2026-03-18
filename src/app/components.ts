6871a4f7f5f7aba95823f7ac0573d399f7db74ca
Kiwi83 <Kwameasenso24@gmail.com>
2026-03-17 23:12:47 -0400

    feat: navbar and sidebar components

---
+++ src/app/components.ts
@@ -0,0 +1,42 @@
+const components = [
+  {
+    componentType: 'Environmental Switch',
+    description: 'An environmental switch with hover animation and micro-interactions',
+    category: 'Buttons',
+    definitions: {
+      props: [],
+      type: [],
+      default: [],
+    },
+  },
+  {
+    componentType: 'Tag Setter',
+    description: 'An environmental switch with hover animation and micro-interactions',
+    category: 'Inputs',
+    definitions: {
+      props: [],
+      type: [],
+      default: [],
+    },
+  },
+  {
+    componentType: 'Save Button',
+    description: 'An environmental switch with hover animation and micro-interactions',
+    category: 'Buttons',
+    definitions: {
+      props: [],
+      type: [],
+      default: [],
+    },
+  },
+  {
+    componentType: 'Tool Bar',
+    description: 'An environmental switch with hover animation and micro-interactions',
+    category: 'Tools',
+    definitions: {
+      props: [],
+      type: [],
+      default: [],
+    },
+  },
+];

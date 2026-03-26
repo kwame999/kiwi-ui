338f84ad0865ba1c32093d454260c302296e192b
Kiwi83 <Kwameasenso24@gmail.com>
2026-03-26 13:42:07 -0400

    fix: overflow issue in intro.mdx

--- src/app/docs/[id]/layout.tsx
+++ src/app/docs/[id]/layout.tsx
@@ -4,9 +4,9 @@ export default function Layout({children}:  Readonly<{
   children: React.ReactNode;
 }> ) {
   return (
-    <div className={`flex`}>
+    <div className={`flex h-[calc(100vh-49px)]`}>
         <SideBar></SideBar>
-        <main className={`flex-1`}>
+        <main className={`flex-1 overflow-y-auto`}>
           {children}
         </main>
         

54f9ff75ccb2ccd7ca10eff5df8e387cf5f881dd
Kiwi83 <Kwameasenso24@gmail.com>
2026-03-29 16:21:40 -0400

    feat: initial commit

---
+++ registry/new-york/hello-world/hello-world.tsx
@@ -0,0 +1,3 @@
+export function HelloWorld() {
+  return <div className="text-red-500">Hello from Kiwi!</div>
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import NavigationBar from "@/components/site/NavigationBar";
 import "./globals.css";
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -13,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" data-theme="dark">
+    <html lang="en" data-theme="dark" className={cn("font-sans", geist.variable)}>
       <body className="flex h-dvh flex-col overflow-hidden antialiased">
         <NavigationBar></NavigationBar>
         <main className="min-h-0 flex-1">{children}</main>
